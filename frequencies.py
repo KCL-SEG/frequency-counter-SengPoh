467dd22f665ba33c10e3fd12ebab68baab81d267
k22002448 <k22002448@kcl.ac.uk>
2023-10-04 18:32:00 +0100

    Complete task

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+import numbers
 
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for key in items:
+        if isinstance(key, numbers.Number): 
+            key = str(key)
+        if (key in frequencies): 
+            frequencies[key] = frequencies[key] + 1
+        else :
+            frequencies[key] = 1
+
     return frequencies
